fac96c120231fbbef5bb67fc0f55f1c6752c4e81
Muhammad Adil Mehmood <iamadilmalick@gmail.com>
2022-12-05 23:19:39 +0500

    header welcome image fixed

--- lib/views/authentication/login.dart
+++ lib/views/authentication/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:river_central/controllers/app_colors.dart';
 
 class Login extends StatelessWidget {
@@ -9,22 +10,41 @@ class Login extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.primary,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Stack(
               children: [
-                Column(
-                  children: [
-                    Image.asset('assets/images/login_background_image.png'),
-                    Text(
-                      'Welcome Back',
-                      style: TextStyle(
-                          fontFamily: 'Raleway',
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
+                Positioned(
+                  right: 40,
+                  left: 40,
+                  top: 0,
+                  bottom: 60,
+                  child: Image.asset(
+                    'assets/images/login_background_image.png',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                    child: Text(
+                      'Welcome back',
+                      style: GoogleFonts.raleway(
+                        fontSize: 54.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        height: 0.9,
+                        shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(3.0, 3.0),
+                            blurRadius: 1.5,
+                            color: Color.fromARGB(255, 65, 65, 65),
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
+                  ),
                 ),
               ],
             ),
@@ -35,55 +55,49 @@ class Login extends StatelessWidget {
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
                 ),
               ),
               child: Padding(
                 padding: const EdgeInsets.symmetric(
-                    vertical: 25.0, horizontal: 50.0),
+                  vertical: 40.0,
+                  horizontal: 50.0,
+                ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     Text(
                       'Login',
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.bold),
+                      style: GoogleFonts.raleway(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
+                    const SizedBox(height: 30.0),
                     Row(
                       children: [
-                        Icon(
-                          Icons.email_outlined,
-                          color: Colors.black45,
-                        ),
-                        SizedBox(
-                          width: 12.0,
-                        ),
+                        Image.asset("assets/icons/message.png"),
+                        const SizedBox(width: 12.0),
                         Text(
                           'Email',
-                          style: TextStyle(
-                              color: Colors.black45,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16.0),
+                          style: GoogleFonts.raleway(
+                            color: Color(0XFF868686),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16.0,
+                          ),
                         ),
                       ],
                     ),
                     TextField(),
-                    SizedBox(
-                      height: 30.0,
-                    ),
+                    const SizedBox(height: 30.0),
                     Row(
                       children: [
                         Icon(
                           Icons.lock_outline_rounded,
                           color: Colors.black45,
                         ),
-                        SizedBox(
-                          width: 12.0,
-                        ),
+                        SizedBox(width: 12.0),
                         Text(
                           'Password',
                           style: TextStyle(
